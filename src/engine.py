cadaa4506f1cb3417a958d70890f47b80259b5cc
Houssam <houssam@infine.io>
2022-06-03 11:48:30 +0200

    add list tags util

--- src/engine.py
+++ src/engine.py
@@ -18,7 +18,18 @@ def getProfileRecommendation(ListProfileId, CloseRecommendation):
     ListRecommendationsId = [row['_id']
                              for index, row in CloseRecommendation.iterrows()]
 
-    # get data
+    # get tags util in this region:
+    List_tags_util = []
+    for index, row in CloseRecommendation.iterrows():
+        if ('tagIds' in row.keys()):
+            tagIds = row['tagIds']
+            for tag_id in tagIds:
+                if tag_id not in List_tags_util:
+                    List_tags_util.append(tag_id)
+
+    # get list categorie utile :
+
+        # get data
     DataFrame_Recommendation = CloseRecommendation
     DataFrame_GuestReviewsScore = ServiceData.get_guestReviews({"_id.recommendationId": {
         "$in": ListRecommendationsId}, "_id.guestId": {"$in": ListProfileId}})
@@ -50,6 +61,11 @@ def getProfileRecommendation(ListProfileId, CloseRecommendation):
         RecommendationList.append(DATA)
 
     return RecommendationList
+
+
+# boucle on recommendation :
+# bulde les id of rezcommendation
+# and tableau of tag
 
 
 def get_big_score(LIST_IDs_OF_GUEST_IN_PROFILE_ON_REGION, recommendation_id, DataFrame_GuestReviewsScore, DataFrame_Recommendation, DataFrame_GuestTags, DataFrame_GuestCategorie):
@@ -113,7 +129,7 @@ def getScore_BEHAVIOR_byID(guest_id, recommendation_id, DataFrame_GuestReviewsSc
 def getScore_LIKE_byID(guest_id, recommendation_id, DataFrame_Recommendation):
     for index, row in DataFrame_Recommendation.iterrows():
         recommendationId = row['_id']
-        if (str(recommendationId) == str(recommendation_id)):
+        if (str(recommendationId) == str(recommendation_id) and 'bookingWhichLikes' in row.keys()):
             bookingWhichLikes = row['bookingWhichLikes']
             if(isinstance(bookingWhichLikes, list) and bookingWhichLikes != None):
                 for _id in bookingWhichLikes:
