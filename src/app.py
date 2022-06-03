4cb36cec9866a419e7b1fbb529a3f22514322ee1
Houssam <houssam@infine.io>
2022-06-03 10:36:01 +0200

    working on scoring and remove flask

--- src/app.py
+++ src/app.py
@@ -1,31 +1,16 @@
 import time
 
 
-from flask import  Flask
-
 from service import Service
-
 import schedule
 import batch
 from database import Database
 import environement
-
-
 Database.initialize()
-
-
-
-app = Flask(__name__)
-
+print('RUUUUUUUUUUUUUUUn')
 
 # Service data
 ServiceData = Service()
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -33,10 +18,6 @@ if __name__ == "__main__":
     schedule.every().day.at(environement.BATCH_HOUR).do(batch.run)
 
     while True:
+        print('STAAART')
         schedule.run_pending()
-        time.sleep(1)
-
-    app.run(debug=False)
-
-
-
+        time.sleep(1000)
