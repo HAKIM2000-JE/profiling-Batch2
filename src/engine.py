6ef4c015784d2cfd5bcf868b82b67ece40396361
Abdelhakim Jebabra <59029617+HAKIM2000-JE@users.noreply.github.com>
2022-06-03 14:48:50 +0100

    Update engine.py

--- src/engine.py
+++ src/engine.py
@@ -18,180 +18,106 @@ def getProfileRecommendation(ListProfileId, CloseRecommendation):
     ListRecommendationsId = [row['_id']
                              for index, row in CloseRecommendation.iterrows()]
 
-    # get tags util in this region:
     List_tags_util = []
+    List_category_util = []
     for index, row in CloseRecommendation.iterrows():
+        # get tags util
         if ('tagIds' in row.keys()):
             tagIds = row['tagIds']
             for tag_id in tagIds:
                 if tag_id not in List_tags_util:
                     List_tags_util.append(tag_id)
 
-    # get list categorie utile :
+        if ('category' in row.keys()):
+            category = row['category']
+            if (category not in List_category_util and category != None):
+                List_category_util.append(category)
 
-        # get data
-    DataFrame_Recommendation = CloseRecommendation
+
+
     DataFrame_GuestReviewsScore = ServiceData.get_guestReviews({"recommendationId": {
         "$in": ListRecommendationsId}, "guestId": {"$in": ListProfileId}})
 
     DataFrame_GuestTags = ServiceData.get_guestTag(
-        {"guestId": {"$in": ListProfileId}})
+        {"guestId": {"$in": ListProfileId}, "_id": {"$in": List_tags_util}})
+
+    print(DataFrame_GuestTags.columns)
+
     DataFrame_GuestCategorie = ServiceData.get_guestCategory(
-        {"guestId": {"$in": ListProfileId}})
+        {"guestId": {"$in": ListProfileId}, "category": {"$in": List_category_util}})
 
-    # get score of behvior :
+    print(DataFrame_GuestCategorie.columns)
 
-    # get score of like :
-    # get  score of tags :
-    # get score of categorie :
-
-    # get moyenne of score : is the big score
-
-    # on boucle sur les Bonne Adreses
-    RecommendationList = []
     for index, recommendation in CloseRecommendation.iterrows():
-        recommendation_id = recommendation['_id']
         DATA = {}
-        DATA['Recommendation Id'] = recommendation['_id']
-        DATA['poi'] = recommendation['poi']
+        scoreLike=0
+        scoreTag=0
+        scoreCategory=0
+        for index, row in DataFrame_GuestReviewsScore.iterrows():
+            if recommendation['_id'] == row['recommendationId']:
+                DATA['Recommendation Id'] = recommendation['_id']
+                DATA['poi'] = recommendation['poi']
+                scoreBehavior = getScore(row['nbClickRecoCard'],
+                                         row['nbClickRecoMarker'], \
+                                         row['nbClickRecoWebSite'],
+                                         row['nbClickRecoDirection'],
+                                         row['clickOnSliderPictures'])
 
-        # calcule du score
-        DATA['SCORE'] = get_big_score(ListProfileId, recommendation_id, DataFrame_GuestReviewsScore,
-                                      DataFrame_Recommendation, DataFrame_GuestTags, DataFrame_GuestCategorie)
-        RecommendationList.append(DATA)
+                if 'bookingWhichLikes' in recommendation.keys():
+                    for _id in recommendation['bookingWhichLikes']:
+                        if _id in ListProfileId :
+                            scoreLike= scoreLike + environement.SCORE_LIKE
 
-    return RecommendationList
+                for index , row in DataFrame_GuestCategorie.iterrows():
+                    scoreCategory =scoreCategory+  row['nbClickCategory']
 
-
-# boucle on recommendation :
-# bulde les id of rezcommendation
-# and tableau of tag
-
-
-def get_big_score(LIST_IDs_OF_GUEST_IN_PROFILE_ON_REGION, recommendation_id, DataFrame_GuestReviewsScore, DataFrame_Recommendation, DataFrame_GuestTags, DataFrame_GuestCategorie):
-    LIST_OF_SCORE = []
-    for guest_id in LIST_IDs_OF_GUEST_IN_PROFILE_ON_REGION:
-
-        # get score of behavior
-        BEHAVIOR_SCORE = getScore_BEHAVIOR_byID(
-            guest_id, recommendation_id, DataFrame_GuestReviewsScore)
-        # get score of like
-        LIKE_SCORE = getScore_LIKE_byID(
-            guest_id, recommendation_id, DataFrame_Recommendation)
-        # get score of tag
-        TAG_SCORE = getScore_TAG_byID(
-            guest_id, recommendation_id, DataFrame_GuestTags, DataFrame_Recommendation)
-        # get score of categorie :
-        CATEGORIE_SCORE = getScore_CATEGORIE_byID(
-            guest_id, recommendation_id, DataFrame_GuestCategorie, DataFrame_Recommendation)
-        print("Calculate Global scoring")
-        MOY_SCORE = (BEHAVIOR_SCORE+LIKE_SCORE+TAG_SCORE+CATEGORIE_SCORE)/4
-        LIST_OF_SCORE.append(MOY_SCORE)
-    return max(LIST_OF_SCORE)
+                for index , row in  DataFrame_GuestTags.iterrows():
+                    scoreCategory = scoreCategory + row['nbClickTag']
 
 
-def getScore_BEHAVIOR_byID(guest_id, recommendation_id, DataFrame_GuestReviewsScore):
-    # No bugs
-    # Score Data
-    print("calculate Behavior Scoring")
-    NB_score = environement.SCORE_nbClickRecoCard + environement.SCORE_nbClickRecoMarker+environement.SCORE_nbClickRecoWebSite + \
-        environement.SCORE_nbClickRecoDirection + \
-        environement.SCORE_clickOnSliderPictures
-    for index, row in DataFrame_GuestReviewsScore.iterrows():
-        if(str(guest_id) == str(row['guestId']) and str(recommendation_id) == str(row['recommendationId'])):
-            # initialise sum to zero:
-            SUM_MOY = 0
+                DATA['SCORE']= (scoreTag+ scoreCategory + scoreBehavior + scoreLike)/4
+                List.append(DATA)
+    RecommendationList=[]
+    for element in List:
 
-            SCORE_nbClickRecoCard = row['nbClickRecoCard'] * \
-                environement.SCORE_nbClickRecoCard
-
-            SCORE_nbClickRecoMarker = row['nbClickRecoMarker'] * \
-                environement.SCORE_nbClickRecoMarker
-
-            SCORE_nbClickRecoWebSite = row['nbClickRecoWebSite'] * \
-                environement.SCORE_nbClickRecoWebSite
-
-            SCORE_nbClickRecoDirection = row['nbClickRecoDirection'] * \
-                environement.SCORE_nbClickRecoDirection
-
-            SCORE_clickOnSliderPictures = row['clickOnSliderPicturesScore'] * \
-                environement.SCORE_clickOnSliderPictures
-
-            # Summ all scores :
-            SUM_MOY = SCORE_nbClickRecoCard + SCORE_nbClickRecoMarker+SCORE_nbClickRecoWebSite + \
-                SCORE_nbClickRecoDirection+SCORE_clickOnSliderPictures
-
-            # create Score Beheiver :
-            SCORE_BEHAVIOR = (SUM_MOY/NB_score)*100
-            return int(SCORE_BEHAVIOR)
-    return 0
+        RecommendationData = {}
 
 
-def getScore_LIKE_byID(guest_id, recommendation_id, DataFrame_Recommendation):
-    print("calculate Likes Scoring")
-    for index, row in DataFrame_Recommendation.iterrows():
-        recommendationId = row['_id']
-        if (str(recommendationId) == str(recommendation_id) and 'bookingWhichLikes' in row.keys()):
-            bookingWhichLikes = row['bookingWhichLikes']
-            if(isinstance(bookingWhichLikes, list) and bookingWhichLikes != None):
-                for _id in bookingWhichLikes:
-                    if(str(_id) == str(guest_id)):
-                        return 1
-    return 0
+        RecommendationData['Recommendation Id'] = element['Recommendation Id']
+
+        RecommendationData['poi'] = element['poi']
+        RecommendationData['score'] = element['SCORE']
 
 
-def getScore_TAG_byID(guest_id, recommendation_id, DataFrame_GuestTags, DataFrame_Recommendation):
-    print("calculate Tags Scoring")
-    for index, row_recommendation in DataFrame_Recommendation.iterrows():
-        if(str(row_recommendation) == str(recommendation_id)):
-            if 'tagIds' in row_recommendation.keys():
-                tagIds = row_recommendation['tagIds']
-                SCORE_TAG = 0
-                TAGS_LEN = 0
-                for index, row_guest_tag in DataFrame_GuestTags.iterrows():
-                    if ((str(guest_id) == str(row_guest_tag['guestId'])) and (row_guest_tag['tagId'] in tagIds)):
-                        SCORE_TAG = SCORE_TAG + row_guest_tag['nbClickTag']
+        RecommendationList.append(RecommendationData)
 
-                return SCORE_TAG/TAGS_LEN
-            else:
-                return 0
-    return 0
+    #afiichage par score decroissant
 
 
-def getScore_CATEGORIE_byID(guest_id, recommendation_id, DataFrame_GuestCategorie, DataFrame_Recommendation):
-    print("calculate Categorie Scoring")
-    for index, row_recommendation in DataFrame_Recommendation.iterrows():
-        if(str(row_recommendation) == str(recommendation_id)):
-            if 'category' in row_recommendation.keys():
-                category = row_recommendation['category']
-                SCORE_CATEGORIE = 0
-                for index, row_guest_categorie in DataFrame_GuestCategorie.iterrows():
-                    if (str(guest_id) == str(row_guest_categorie['guestId'])) and (category == row_guest_categorie['category']):
-                        print('CALCULE DES CATEGORY')
-                        SCORE_CATEGORIE = row_guest_categorie['nbClickCategory']
-                        return SCORE_CATEGORIE
-                return SCORE_CATEGORIE
-            else:
-                return 0
-    return 0
+    return   RecommendationList
 
 
-# def getScore(nbClickRecoCard, nbClickRecoMarker, nbClickRecoWebSite, nbClickRecoDirection, clickOnSliderPictures):
-#     NB_score = environement.SCORE_nbClickRecoCard + environement.SCORE_nbClickRecoMarker + environement.SCORE_nbClickRecoWebSite + \
-#         environement.SCORE_nbClickRecoDirection + \
-#         environement.SCORE_clickOnSliderPictures
-
-#     score = nbClickRecoMarker * environement.SCORE_nbClickRecoMarker + nbClickRecoCard*environement.SCORE_nbClickRecoCard +\
-#         nbClickRecoDirection * environement.SCORE_nbClickRecoDirection + \
-#         nbClickRecoWebSite*environement.SCORE_nbClickRecoDirection
-
-#     return score / NB_score
 
 
-# def getScoreOnSliderPictures(clickOnSliderPictures):
-#     print(clickOnSliderPictures)
 
-#     if clickOnSliderPictures.bool() == True:
-#         return environement.SCORE_clickOnSliderPictures
-#     else:
-#         return 0
+
+def getScore(nbClickRecoCard,nbClickRecoMarker,nbClickRecoWebSite,nbClickRecoDirection , clickOnSliderPictures):
+    NB_score = environement.SCORE_nbClickRecoCard + environement.SCORE_nbClickRecoMarker + environement.SCORE_nbClickRecoWebSite + \
+               environement.SCORE_nbClickRecoDirection + \
+               environement.SCORE_clickOnSliderPictures
+
+    score = nbClickRecoMarker* environement.SCORE_nbClickRecoMarker + nbClickRecoCard*environement.SCORE_nbClickRecoCard+\
+            nbClickRecoDirection* environement.SCORE_nbClickRecoDirection + nbClickRecoWebSite*environement.SCORE_nbClickRecoDirection
+
+    return score /NB_score
+
+
+
+def getScoreOnSliderPictures(clickOnSliderPictures):
+    print(clickOnSliderPictures)
+
+    if clickOnSliderPictures.bool()==True:
+        return environement.SCORE_clickOnSliderPictures
+    else:
+        return 0
+
